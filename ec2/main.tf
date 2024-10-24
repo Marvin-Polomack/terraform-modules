99091b458f203f72ad136372ab44e5a83d06a43f
mpolomack <marvin.polomack@gmail.com>
2024-10-24 11:59:53 +0200

    Create EC2 module

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2_k8s" {
+resource "aws_instance" "ec2" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
