88772037f85ad2d08214a3c90095565f7ce531e6
mpolomack <marvin.polomack@gmail.com>
2024-12-21 00:06:59 +0100

    Fix ALB module

--- alb/main.tf
+++ alb/main.tf
@@ -87,11 +87,3 @@ resource "aws_lb_target_group" "tg" {
     unhealthy_threshold = 3
   }
 }
-
-# Register EC2 Instances with Target Group
-resource "aws_lb_target_group_attachment" "tg_attachment" {
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = var.target_id  # Replace with your Kubernetes EC2 instance ID
-  port             = 3000
-}
-
